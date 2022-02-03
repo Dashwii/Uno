f813731e918bf6e58d4b4127ae0fdbbe760ddae4
Dashwii <dashwiibox@gmail.com>
2022-02-03 06:54:03 -0500

    added card system

--- cards.py
+++ cards.py
@@ -1,3 +1,23 @@
-def generate_cards():
+import random 
+
+def return_deck():
     colors = ["red", "blue", "green", "yellow"]
-    numbers = [i for i in range(10)]
+    specials = ["skip", "reverse", "draw"]
+    deck = []
+    random.shuffle(deck)
+    for color in colors:  # Append 0-9 color cards
+        for number in range(0, 10):
+            deck.append((color, number))
+    for color in colors:  # Append 1-9 color cards 
+        for number in range(1, 10):
+            deck.append((color, number))
+    for color in colors:  # Append special cards
+        for special in specials:
+            deck.append((color, special))
+            deck.append((color, special))
+    for _ in range(4):    # Append black cards
+        deck.append(("black", "4 wild"))
+        deck.append(("black", "4 wild draw"))
+    random.shuffle(deck)
+    return deck 
+ 
