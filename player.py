6c37e9d0e6900daa6c540772ab2561fe46cf4862
Dashwii <dashwiibox@gmail.com>
2022-02-05 18:41:41 -0500

    player can now pick up cards and use wild cards

--- player.py
+++ player.py
@@ -3,11 +3,14 @@ class Player:
         self.name = name
         self.deck = deck
     
-    def play(self, current_gamedeck_color, current_gamedeck_number):
+    def play(self, game_deck, current_gamedeck_color, current_gamedeck_number):
         print(f"Your cards are:")
         for index, card in enumerate(self.deck):
-            if index == 0: 
-                print(f"{index + 1} {card}", end=", ")
+            if index == 0:
+                if len(self.deck) == 1:
+                    print(f"{index + 1} {card}")
+                else:
+                    print(f"{index + 1} {card}", end=", ")
             elif index == len(self.deck) - 1:
                 print(f"{index + 1} {card}")
             else:
@@ -16,28 +19,38 @@ class Player:
         while True:
             response = input("> ")
             try:
-                response = int(response)
-                response -= 1
-                chosen_card = self.deck[response]
+                card_index = int(response)
+                card_index -= 1
+                chosen_card = self.deck[card_index]
             except ValueError as e:
-                if response == "P":
-                    self.take_card() 
+                if response == "m":
+                    return "m"
+                if response == "p":
+                    return "pickup" 
                 else:
-                    continue 
-            if not response <= len(self.deck) - 1:
+                    continue
+            if not card_index <= len(self.deck) - 1:
                print("Not a valid card")
                continue
             elif chosen_card[0] == "black":  # Black cards can be played regardless 
                 print(f"Played {chosen_card}")
-                return response, chosen_card
+                while True:
+                    colors = ["red", "green", "blue", "yellow"]
+                    print("Select the color you want!: 1. Red, 2. Green. 3. Blue, 4. Yellow")
+                    new_color = int(input(">"))
+                    if new_color not in (1, 2, 3, 4):
+                        continue
+                    else:
+                        chosen_card[2] = colors[new_color - 1]
+                    return card_index, chosen_card
             elif current_gamedeck_color is None and current_gamedeck_number is None:
                 print(f"Played {chosen_card}")
-                return response, chosen_card 
+                return card_index, chosen_card 
             elif chosen_card[0] != current_gamedeck_color and chosen_card[1] != current_gamedeck_number:  # Check if card has the same number or color as current played card
                 print("The card you chose is not playable!")
             else:
                 print(f"Played {chosen_card}")
-                return response, chosen_card
+                return card_index, chosen_card
                 
     def take_card(self, deck):
         self.deck.append(deck.pop())
