d8e6f5628a5dd9ab530f41bd0dbbce167382b07d
Dashwii <dashwiibox@gmail.com>
2022-03-14 13:00:59 -0400

    player rework

--- player.py
+++ player.py
@@ -1,64 +1,83 @@
+import pygame
+
+"""
+A player/ai moveset might work better being kept in the class as a statemachine.
+It would cut down on the if elses and hopefully make the code for a player/ai move clearer to follow.
+"""
 class Player:
     def __init__(self, name, deck):
+        self.controllable = True
         self.name = name
         self.deck = deck
-    
-    def play(self, game_deck, current_gamedeck_color, current_gamedeck_number):
-        print(f"Your cards are:")
-        for index, card in enumerate(self.deck):
-            if index == 0:
-                if len(self.deck) == 1:
-                    print(f"{index + 1} {card}")
+        self.threat_level = 0
+        self.current_hovered_card_index = 0
+        self.move_ticker = 0
+        self.status = None
+        self.state = None
+        self.decision = None
+
+    def input(self, current_board_color, current_board_type):
+        if self.decision is not None:
+            return
+        self.move_ticker += 1
+        keys = pygame.key.get_pressed()
+        if self.move_ticker > 10:
+            if keys[pygame.K_LEFT]:
+                if self.current_hovered_card_index - 1 < 0:
+                    self.current_hovered_card_index = len(self.deck) - 1
                 else:
-                    print(f"{index + 1} {card}", end=", ")
-            elif index == len(self.deck) - 1:
-                print(f"{index + 1} {card}")
-            else:
-                print(f"{index + 1} {card}", end=", ")
-        print("What card do you want to play? If none of your cards are playable type P to pick up a card!")
-        while True:
-            response = input("> ")
-            try:
-                card_index = int(response)
-                card_index -= 1
-                chosen_card = self.deck[card_index]
-            except ValueError as e:
-                if response == "m":
-                    return "m"
-                if response == "p":
-                    return "pickup" 
+                    self.current_hovered_card_index -= 1
+            if keys[pygame.K_RIGHT]:
+                if self.current_hovered_card_index + 1 > len(self.deck):
+                    self.current_hovered_card_index = 0
                 else:
-                    continue
-            if not card_index <= len(self.deck) - 1:
-               print("Not a valid card")
-               continue
-            elif chosen_card[0] == "black":  # Black cards can be played regardless 
-                print(f"Played {chosen_card}")
-                while True:
-                    colors = ["red", "green", "blue", "yellow"]
-                    print("Select the color you want!: 1. Red, 2. Green. 3. Blue, 4. Yellow")
-                    new_color = int(input(">"))
-                    if new_color not in (1, 2, 3, 4):
-                        continue
+                    self.current_hovered_card_index += 1
+            if keys[pygame.K_p]:
+                self.decision = "pickup"
+            if keys[pygame.K_SPACE]:
+                if self.deck[self.current_hovered_card_index].card_type == "wild draw":
+                    self.decision = self.deck[self.current_hovered_card_index]
+                elif self.deck[self.current_hovered_card_index].card_type == "wild" and not self.threat_level:
+                    self.decision = self.deck[self.current_hovered_card_index]
+                elif current_board_color is None:
+                    self.decision = self.deck[self.current_hovered_card_index]
+                else:
+                    if self.threat_level:
+                        if self.deck[self.current_hovered_card_index].card_type == "draw":
+                            self.decision = self.deck[self.current_hovered_card_index]
                     else:
-                        chosen_card[2] = colors[new_color - 1]
-                    return card_index, chosen_card
-            elif current_gamedeck_color is None and current_gamedeck_number is None:
-                print(f"Played {chosen_card}")
-                return card_index, chosen_card 
-            elif chosen_card[0] != current_gamedeck_color and chosen_card[1] != current_gamedeck_number:  # Check if card has the same number or color as current played card
-                print("The card you chose is not playable!")
-            else:
-                print(f"Played {chosen_card}")
-                return card_index, chosen_card
-                
-    def take_card(self, deck):
-        self.deck.append(deck.pop())
-    
-    def remove_card(self, index):
-        card = self.deck.pop(index)
-        return card 
-    
+                        if self.deck[self.current_hovered_card_index].card_color == current_board_color or self.deck[self.current_hovered_card_index].card_type == current_board_type:
+                            self.decision = self.deck[self.current_hovered_card_index]
+
+            self.move_ticker = 0
+
+    def sort_cards_visual(self):
+        red_cards = []
+        green_cards = []
+        blue_cards = []
+        yellow_cards = []
+        black_cards = []
+        for card in self.deck:
+            if card.card_color == "red":
+                red_cards.append(card)
+            if card.card_color == "green":
+                green_cards.append(card)
+            if card.card_color == "blue":
+                blue_cards.append(card)
+            if card.card_color == "yellow":
+                yellow_cards.append(card)
+            if card.card_color == "black":
+                black_cards.append(card)
+        red_cards.sort(key=lambda card: card.sort_num)
+        green_cards.sort(key=lambda card: card.sort_num)
+        blue_cards.sort(key=lambda card: card.sort_num)
+        yellow_cards.sort(key=lambda card: card.sort_num)
+        black_cards.sort(key=lambda card: card.sort_num)
+        self.deck = red_cards + green_cards + blue_cards + yellow_cards + black_cards
+
+    def choose_new_color_wild(self, card_index, chosen_card):
+        pass
+
     def __repr__(self):
         return self.name 
  
