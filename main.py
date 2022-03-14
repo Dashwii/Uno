3cedea1c7a9a0554aac5ef3b883269498a11a91c
Dashwii <dashwiibox@gmail.com>
2022-03-14 13:02:12 -0400

    main

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-def main():
-    pass 
+from init_stuff import *
+from game_logic import Board
+from render_helper import update_card_positions
+
+
+
+class Game:
+    def __init__(self):
+        self.display = pygame.display.get_surface()
+        self.clock = pygame.time.Clock()
+        self.events = []
+        self.board = Board(self)
+
+    def run(self):
+        while True:
+            if self.board.reset:
+                self.board = Board(self)
+            self.handle_events()
+            self.display.blit(BACKGROUND, (0, 0))
+            self.board.play()
+            pygame.display.update()
+            self.clock.tick(FPS)
+            self.event_clear()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            self.events.append(event)
+
+    def event_clear(self):
+        self.events = []
 
 
 if __name__ == "__main__":
-    main()
-    
+    game = Game()
+    game.run()
