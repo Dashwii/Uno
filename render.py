5d8c04c9a42b34310e93471340af27781351755a
Dashwii <dashwiibox@gmail.com>
2022-04-04 07:47:16 -0400

    changed rendering used deck

--- render.py
+++ render.py
@@ -1,20 +1,31 @@
 from init_stuff import *
 from render_helper import *
+from random import randrange
 
 
 color_map = {"red": RED, "green": GREEN, "blue": BLUE, "yellow": YELLOW, "black": WHITE}
 
 
-class CardRendering:
+class BoardRendering:
     def __init__(self, board):
         self.board = board
         self.rotation_list = self.board.rotation_list
         self.display = pygame.display.get_surface()
         self.camera_pov_index = self.board.camera_pov_index
-        self.name_renders = create_ai_render_names(self.rotation_list, self.camera_pov_index)
+        self.name_renders = create_render_names(self.rotation_list, self.camera_pov_index)
 
         self.moving_cards_leaving = []  # <--- Make this also a list for future jump in mechanic. Will have multiple cards leaving their deck and animated
         self.moving_cards = []  # <--- This is only used in game not during the card dealing state (Gonna try and use this during card dealing state)
+
+    def update_rotation_interface_color(self, current_player_index, last_player_index=None):
+        if current_player_index == self.camera_pov_index:
+            if last_player_index is not None:
+                self.name_renders[last_player_index].update_color("white")
+        else:
+            self.name_renders[current_player_index].update_color("green")
+            if last_player_index != self.camera_pov_index:
+                if last_player_index is not None:
+                    self.name_renders[last_player_index].update_color("white")
 
     def card_leaving_deck_animation(self, card):
         self.moving_cards_leaving.append(card)
@@ -23,6 +34,7 @@ class CardRendering:
     def check_moving_cards_leaving_done(self):
         for i, card in enumerate(self.moving_cards_leaving):
             if card.rect.x == card.destination_x and card.rect.y == card.destination_y:
+                card.rotation = randrange(card.rotation - 35, card.rotation + 35)
                 self.moving_cards_leaving.pop(i)
                 self.board.game_flow.used_deck.append(card)
         if len(self.moving_cards_leaving) == 0:
@@ -39,31 +51,36 @@ class CardRendering:
         if len(self.moving_cards) == 0:
             return True
 
-    def render_cards(self):
+    def render_cards(self, player_color_choosing_state=None):
         # Deck renders
         for i, player in enumerate(self.rotation_list):
             if i == self.camera_pov_index:
                 for card in player.deck:
                     self.display.blit(card_image_map[(card.card_color.title(), str(card.card_type))], card.rect)
-            else:
+            elif not player_color_choosing_state:
                 for card in player.deck:
                     self.display.blit(pygame.transform.rotate(asset_map["Uno Card Back"], card.rotation), card.rect)
 
-        # Render leaving cards
-        for i, card in enumerate(self.moving_cards_leaving):
-            self.display.blit(pygame.transform.rotate(card_image_map[(card.card_color.title(), str(card.card_type))], card.rotation), card.rect)
-        # Render Unused Deck
-        self.display.blit(pygame.transform.rotate(asset_map["Uno Card Back"], 20), (UNUSED_DECK_POSITION[0], UNUSED_DECK_POSITION[1], CARD_WIDTH, CARD_HEIGHT))
+        if player_color_choosing_state:
+            return
+        else:
+            # Render leaving cards
+            for i, card in enumerate(self.moving_cards_leaving):
+                self.display.blit(pygame.transform.rotate(card_image_map[(card.card_color.title(), str(card.card_type))], card.rotation), card.rect)
+            # Render Unused Deck
+            self.display.blit(pygame.transform.rotate(asset_map["Uno Card Back"], 20), (UNUSED_DECK_POSITION[0], UNUSED_DECK_POSITION[1], CARD_WIDTH, CARD_HEIGHT))
+            # Render Used Deck
 
     def render_ai_names(self):
         for name in self.name_renders:
-            name.render()
+            if name is not None:
+                name.render()
 
     def render_used_deck(self, used_deck):
-        last_card = used_deck[-1]
-        if last_card.card_type == "wild":
-            self.display.blit(wild_image_color_map[last_card.wild_color], (WIDTH // 2 - last_card.rect.width // 2, HEIGHT // 2 - last_card.rect.height // 2))
-        elif last_card.card_type == "wild draw":
-            self.display.blit(wild_draw_image_color_map[last_card.wild_color], (WIDTH // 2 - last_card.rect.width // 2, HEIGHT // 2 - last_card.rect.height // 2))
-        else:
-            self.display.blit(card_image_map[(last_card.card_color.title(), str(last_card.card_type))], (WIDTH // 2 - last_card.rect.width // 2, HEIGHT // 2 - last_card.rect.height // 2))
+        for card in used_deck:
+            if card.card_type == "wild":
+                self.display.blit(pygame.transform.rotate(wild_image_color_map[card.wild_color], card.rotation), (WIDTH // 2 - card.rect.width // 2, HEIGHT // 2 - card.rect.height // 2))
+            elif card.card_type == "wild draw":
+                self.display.blit(pygame.transform.rotate(wild_draw_image_color_map[card.wild_color], card.rotation), (WIDTH // 2 - card.rect.width // 2, HEIGHT // 2 - card.rect.height // 2))
+            else:
+                self.display.blit(pygame.transform.rotate(card_image_map[(card.card_color.title(), str(card.card_type))], card.rotation), (WIDTH // 2 - card.rect.width // 2, HEIGHT // 2 - card.rect.height // 2))
