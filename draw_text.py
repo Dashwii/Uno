a3fd890de36319a0e9f0e6818762ac786ee2abe5
Dashwii <dashwiibox@gmail.com>
2022-04-04 07:49:23 -0400

    Refactored DrawText class

--- draw_text.py
+++ draw_text.py
@@ -1,36 +1,23 @@
-import pygame
+from init_stuff import *
 pygame.font.init()
 
 
-class RenderText:
-    def __init__(self, text, pos, color="white", font_size=35, background_color=None):
-        self.text = text
-        self.background = False
-        self.screen = pygame.display.get_surface()
-        self.font_size = font_size
-        self.font = pygame.font.SysFont("arial", self.font_size)
+class DrawText:
+    def __init__(self, text, pos, font_size=35, color="white"):
+        self.display = pygame.display.get_surface()
+        self.text = str(text)
+        self.position = pos
         self.color = color
-        self.text_surface = self.font.render(text, True, self.color)
-        self.text_rect = self.text_surface.get_rect()
-        self.text_rect.x, self.text_rect.y = pos[0] - self.text_rect.width // 2, pos[1] - self.text_rect.height // 2
-        if background_color is not None:
-            self.background = True
-            self.temp_surface = pygame.Surface(self.text_surface.get_size())
-            self.temp_surface.fill(background_color)
+        self.font = pygame.font.SysFont("arial", int(font_size * SCALING_RATIO))
+        self.text_surface = self.font.render(self.text, True, self.color)
 
     def render(self):
-        if self.background:
-            self.screen.blit(self.temp_surface, (self.text_rect.x, self.text_rect.y))
-        self.screen.blit(self.text_surface, (self.text_rect.x, self.text_rect.y))
+        self.display.blit(self.text_surface, (self.position[0], self.position[1]))
 
     def update_color(self, color):
         self.color = color
         self.text_surface = self.font.render(self.text, True, self.color)
 
     def update_text(self, text):
-        self.text = text
-        self.text_surface = self.font.render(text, True, self.color)
-
-
-
-
+        self.text = str(text)
+        self.text_surface = self.font.render(self.text, True, self.color)
